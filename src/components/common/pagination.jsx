f0e8500f0f8e204537b4dc27aa5269b143082975
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-07-12 09:23:35 -0300

    add argument destructuring to pagination. Modify class to className

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = (props) => {
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = itemsCount / pageSize;
+
   return (
     <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item">
-          <a class="page-link">Previous</a>
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link">Previous</a>
         </li>
-        <li class="page-item">
-          <a class="page-link">1</a>
+        <li className="page-item">
+          <a className="page-link">1</a>
         </li>
-        <li class="page-item">
-          <a class="page-link">2</a>
+        <li className="page-item">
+          <a className="page-link">2</a>
         </li>
-        <li class="page-item">
-          <a class="page-link">3</a>
+        <li className="page-item">
+          <a className="page-link">3</a>
         </li>
-        <li class="page-item">
-          <a class="page-link">Next</a>
+        <li className="page-item">
+          <a className="page-link">Next</a>
         </li>
       </ul>
     </nav>
